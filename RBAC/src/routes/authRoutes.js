ac0f1dcfdc475dbb5aac5c36341bcc5bf0df3b2e
vishwa satasiya <vishwa.s@sarvadhi.com>
2025-01-04 08:48:46 +0530

    Clean up and prepare mvc branch

--- RBAC/src/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const route = express.Router();
-const { register, login } = require("../controllers/authController");
-route.post("/register", register);
-route.post("/login", login);
-
-module.exports = route;
